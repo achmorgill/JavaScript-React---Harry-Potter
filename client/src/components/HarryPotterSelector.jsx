6d51c4746817d564728bae4672cdc0691184ae89
Gill Liddle <gillianmliddle49@gmail.com>
2017-07-11 15:08:41 +0100

    got data from Harry Potter api

---
+++ client/src/components/HarryPotterSelector.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class HarryPotterSelector extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: undefined
+    }
+  }
+
+render() {
+
+  
+
+}
+
+
+
+
+}
+
+export default HarryPotterSelector;
--- client/src/containers/HarryPotterContainer.jsx
+++ client/src/containers/HarryPotterContainer.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import HarryPotterSelector from '../components/HarryPotterSelector'
+import {HarryPotterDetails} from '../components/HarryPotterDetails'
 
 class HarryPotterContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: []
+    }
+  }
 
+componentDidMount() {
+  const url = 'http://hp-api.herokuapp.com/api/characters'
+
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = () => {
+    if(request.status !== 200) return
+
+      const jsonString = request.responseText;
+    const data = JSON.parse(jsonString);
+
+    this.setState({ characters: data })
+    console.log(data)
+  }
+  request.send()
+}
 
 render() {
 
   return (
     <div>
-      hello world
+      <h2>Harry Potter Characters</h2>
+     
+      <h2>Details</h2>
+
     </div>
     )
 }
